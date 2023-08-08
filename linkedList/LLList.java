b3772cdf832ffa7ab4d046a3033042fe4c79c7de
prasanth0143 <prasanthajay0011@gmail.com>
2023-08-08 10:06:15 +0530

    Lightly Modified

--- linkedList/LLList.java
+++ linkedList/LLList.java
@@ -1,4 +1,24 @@
 package linkedList;
 
 public class LLList {
+
+    public class Node{
+        Node next;
+        Node head;
+        int size;
+        Node(){
+            int size = 0;
+        }
+
+        public void inserCtion(int num){
+            if(head == null){
+                //node = num;
+            }
+
+
+        }
+
+
+
+    }
 }
